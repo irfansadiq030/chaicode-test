7c6b6e07df4d840a631ad553387047928b7b7bdd
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2024-04-18 11:19:22 +0500

    fixes error port

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${process.env.PORT}`)
 })
